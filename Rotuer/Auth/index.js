8fbcefd61216d00713bd24e31dcac855059f466a
kimbogyu <bg0820@naver.com>
2020-04-06 18:47:03 +0900

    #7 회원가입 라우터 추가

---
+++ Rotuer/Auth/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const pool = require('../../DB');
+const crypto = require('crypto');
+
+// GET
+// GET  req.query
+// POST req.body
+
+// 학생 게정 생성 코드
+router.post('/register', async function(req, res) {
+	
+});
+
+
+module.exports = router;
