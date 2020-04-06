4f1ea8b4e30c4dfa77027a5c2c344e9db784969e
kimbogyu <bg0820@naver.com>
2020-04-06 18:47:39 +0900

    #7 회원가입 DB 연동

--- Rotuer/Auth/index.js
+++ Rotuer/Auth/index.js
@@ -8,7 +8,30 @@ const crypto = require('crypto');
 
 // 학생 게정 생성 코드
 router.post('/register', async function(req, res) {
-	
+	const {id, pw, name, email} = req.body;
+
+	// 비밀번호 sha256 방식으로 해시화
+	var _pw_sha256_hash = crypto.createHash('sha256').update(pw).digest('hex');
+
+	let con;
+	try {
+		con = await pool.getConnection();
+
+		const query = "INSERT INTO user (id, pw, name, email, userType) values (?, ?, ?, ?, 0)";
+		
+		await pool.query(con, query, [id, _pw_sha256_hash, name, email]);
+
+		res.send({msg: '회원가입 성공'});
+
+	} catch (error) {
+		console.log('에러났을때 처리하는 부분', error);
+		if(error.errno === 1062) {
+			res.send({msg: '이미 가입되어있는 아이디 입니다.'});
+		} else
+			res.send({msg: '알수없는 에러 실패'});
+	} finally {
+		con.release();
+	}
 });
 
 router.post('/login', async function(req, res) {
