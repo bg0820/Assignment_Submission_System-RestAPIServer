1904ab9aeaf0efeb99e4ef7f4f5bbe5f1d636a17
JIHEE <rlagus1614@naver.com>
2020-04-11 16:01:23 +0900

    #13 과제 생성 라우터 추가

---
+++ Rotuer/Task/index.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const pool = require('../../DB');
+
+// GET
+// GET  req.query
+// POST req.body
+
+
+// TODO : 
+/*
+ 과제 생성 [ 교수 ]
+ 과제 목록 [ 교수 ]
+ 과제 목록 [ 학생 ]
+
+*/
+
+router.post('/create', async function (req, res) {
+    
+});
+
+module.exports = router;
