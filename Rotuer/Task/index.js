ff49f4f3957e9a574cf0f32cd82a9f646f37f53b
JIHEE <rlagus1614@naver.com>
2020-04-13 20:43:29 +0900

    #15 과제 목록[학생]  DB연동 및 로직 구현

--- Rotuer/Task/index.js
+++ Rotuer/Task/index.js
@@ -80,7 +80,31 @@ router.get('/list/professor', async function(req, res) {
 
 
 router.get('/list/student', async function(req, res) {
-    
+    const {userIdx} = req.body;
+    console.log("요청들어온 userIdx : ",userIdx);
+
+	let con;
+	try {
+		con = await pool.getConnection();
+
+		const query =   'select taskIdx,directory,score,language,submissionDate from evaluation where userIdx = ?';
+        
+        const list = await pool.query(con, query, [userIdx]);
+
+		res.send({
+            msg: '조회 성공',
+            list: list
+        });
+
+	} catch (error) {
+		console.log('에러났을때 처리하는 부분', error);
+		// if(error.errno === 1062) {
+		// 	res.send({msg: '이미 개설된 강의 입니다.'});
+		// } else
+			res.send({msg: '알수없는 에러 실패'});
+	} finally {
+		// con.release();
+	}
 });
 
 
