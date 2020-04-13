fab3b328c193ce0fa2619c9b186ac6606283ed04
kimbogyu <bg0820@naver.com>
2020-04-13 11:02:00 +0900

    #3, #4, #11, #17 학생 이 제출한 코드를 컴파일 하기 위해 학생 전용 디렉토리에 업로드

--- Models/Builder/index.js
+++ Models/Builder/index.js
@@ -5,6 +5,44 @@ class Builder {
 
 	}
 
+	/*
+	* userDirectory = 학번 또는 교번
+	* taskDirectory = 과제 아이디 이름의 폴더
+	* fileName = 컴파일 할 파일 명
+	* code = 실제 코드 내용
+	*/
+	studentInit = (userDirectory, taskDirectory, fileName, code) => {
+		try {
+			if(!fs.existsSync(userDirectory)) 
+				fs.mkdirSync(userDirectory, {
+					recursive: true,
+					mode: 0o777
+				});
+
+			if(!fs.existsSync(taskDirectory)) 
+				fs.mkdirSync(taskDirectory, {
+					recursive: true,
+					mode: 0o777
+				});
+
+			// Input.c, c++, java 존재하면 삭제
+			if(fs.existsSync(taskDirectory+ '/' + fileName)) 
+				fs.unlinkSync(taskDirectory + '/' + fileName);
+
+			fs.writeFileSync(taskDirectory + '/' + fileName, code, {
+				encoding: 'utf8',
+				mode: 0o777,
+				flag: 'w'
+			});
+
+			return taskDirectory + '/' + fileName;
+		} catch(err) {
+			console.log('파일 생성 에러', err);
+		}
+
+		return '';
+	}
+
 }
 
 const builder = new Builder();
