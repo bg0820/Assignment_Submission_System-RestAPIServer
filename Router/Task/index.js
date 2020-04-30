68894616e623dfa2ff82560e5a83704f401fe153
kimbogyu <bg0820@naver.com>
2020-04-30 20:43:40 +0900

    불필요한 코드 및 주석 제거

--- Router/Task/index.js
+++ Router/Task/index.js
@@ -22,6 +22,7 @@ router.use(function(req, res, next){
 			res.status(400).send({result: 'failed', msg: '유효하지 않은 토큰 입니다.'});
 		else {
 			req.decode = decoded;
+			console.log(req.decode);
 			next();
 		}
 	});
@@ -57,9 +58,6 @@ router.post('/create', async function (req, res) {
         res.send({ msg: '과제 생성 성공' });
     } catch (error) {
         console.log('에러났을때 처리하는 부분', error);
-        // if(error.errno === 1062) {
-        // 	res.send({msg: '이미 개설된 강의 입니다.'});
-        // } else
         res.send({ msg: '알수없는 에러 실패' });
     } finally {
         con.release();
@@ -68,8 +66,7 @@ router.post('/create', async function (req, res) {
 });
 
 router.get('/list', async function(req, res) {
-    const decode = req.decode; // {courseIdx} = req.body;
-    console.log(decode);
+    const decode = req.decode;
     
     let con;
     try {
@@ -104,45 +101,13 @@ router.get('/list', async function(req, res) {
         });
     
     } catch (error) {
-        console.log('에러났을때 처리하는 부분', error);
-        // if(error.errno === 1062) {
-        // 	res.send({msg: '이미 개설된 강의 입니다.'});
-        // } else
-            res.send({msg: '알수없는 에러 실패'});
+		console.log('에러났을때 처리하는 부분', error);
+		
+    	res.send({msg: '알수없는 에러 실패'});
     } finally {
-        // con.release();
+        con.release();
     }
 });
-
-/*
-
-router.get('/list/student', async function(req, res) {
-    const {userIdx} = req.body;
-    console.log("요청들어온 userIdx : ",userIdx);
-
-	let con;
-	try {
-		con = await pool.getConnection();
-
-		
-        const list = await pool.query(con, query, [userIdx]);
-
-		res.send({
-            msg: '조회 성공',
-            list: list
-        });
-
-	} catch (error) {
-		console.log('에러났을때 처리하는 부분', error);
-		// if(error.errno === 1062) {
-		// 	res.send({msg: '이미 개설된 강의 입니다.'});
-		// } else
-			res.send({msg: '알수없는 에러 실패'});
-	} finally {
-		// con.release();
-	}
-});*/
-
 
 
 
